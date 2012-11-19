4b206c923449d5aec732f5bfc9e11f666a90213a
Kevin Swarthout <kcswarthout@gmail.com>
2012-11-18 18:42:14 -0600

    typo

--- packet.c
+++ packet.c
@@ -162,7 +162,7 @@ void printIpPacketInfo(struct ip_packet *pkt, struct sockaddr_storage *saddr) {
 	printf("Pri %hhu : Sz %lu : Dst %s %iu : Src %s %iu", pkt->priority, 
 			pkt->length, addrToName(pkt->dest), pkt->destPort, addrToName(pkt->src), pkt->srcPort);
 	
-	printPacketInfo((struct ip_packet *)pkt->payload, saddr);
+	printPacketInfo((struct packet *)pkt->payload, saddr);
     /*
     printf("  Packet from %s:%u (%lu payload bytes):\n",ipstr,ipport,pkt->len);
     printf("    type = %c\n", pkt->type);
--- packet.h
+++ packet.h
@@ -46,6 +46,6 @@ void sendPacketTo(int sockfd, struct packet *pkt, struct sockaddr *addr);
 void sendIpPacketTo(int sockfd, struct ip_packet *pkt, struct sockaddr *addr);
 
 void printPacketInfo(struct packet *pkt, struct sockaddr_storage *saddr);
-
+void printIpPacketInfo(struct ip_packet *pkt, struct sockaddr_storage *saddr);
 #endif
 
